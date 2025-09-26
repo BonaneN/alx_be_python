bcaed7518324e601a32f426befcc75abb5bd721f
BonaneN <rbonane336@gmail.com>
2025-09-26 11:02:06 +0200

    datetime library

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -14,7 +14,7 @@ def main():
     display_current_datetime()
     while True:
         try:
-            days = int(input("Enter number of days to add: "))
+            days = int(input("Enter the number of days to add to the current date: "))
             future_date = calculate_future_date(days)
             print(f"Future date:", future_date.strftime("%Y-%m-%d %H:%M:%S"))
             break
