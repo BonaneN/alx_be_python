c046b5c2134e9ff1a3e64423fb48e7cd7f8195f1
BonaneN <rbonane336@gmail.com>
2025-09-26 11:01:00 +0200

    datetime library

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+
+#Function to display the current date and time
+def display_current_datetime():
+    current_date = datetime.now()
+    print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
+
+# Function to calculate_future_date(days):
+    future_date = datetime.now() + timedelta(days=days)
+    return future_date
+
+#Main function to interact with the user
+def main():
+    display_current_datetime()
+    while True:
+        try:
+            days = int(input("Enter number of days to add: "))
+            future_date = calculate_future_date(days)
+            print(f"Future date:", future_date.strftime("%Y-%m-%d %H:%M:%S"))
+            break
+        except ValueError:
+            print("Please enter a valid number of days.")
+
+#Main function to run the program
+if __name__ == "__main__":
+    main()
